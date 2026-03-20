6c15daf6cbaaf5652636abc10e5f8b8961098a0c
Vaishali Sharma <vs481478@gmail.com>
2026-03-20 17:04:40 +0530

    modify source code

--- sourcecode.v
+++ sourcecode.v
@@ -1,4 +1,47 @@
-DSCH3
-VERSION 3/18/2026 6:59:38 PM
-BB(-15,-15,70,70)
-FFIG C:\Users\ASUS\OneDrive\Energy Source Controller\sourcecode.v
+module home_energy_source_controller (
+input clk ,
+input reset , 
+input ac_on ,
+input fan_on ,
+input wm_on ,
+input bulb_on ,
+input fridge_on , 
+input day_flag , // 1 for day and 0 for night
+input battery_status, 
+output solar_mode ,
+output battery_mode,
+output grid_mode
+    
+);
+
+wire [15:0] total_load ;
+wire [15:0] solar_generation ;
+wire [15:0] energy_consumption ;
+wire [15:0] cost;
+load_calculator load_calc (
+    .ac_on(ac_on),
+    .fan_on(fan_on),
+    .wm_on(wm_on),
+    .bulb_on(bulb_on),
+    .fridge_on(fridge_on),
+    .total_load(total_load)
+);
+solar_generation_calculator solar_calc (
+    .day_flag(day_flag),
+    .solar_generation(solar_generation)
+);
+energy_consumption_calculator energy_calc (
+    .total_load(total_load),
+    .energy_consumption(energy_consumption),
+    .cost(cost)
+);
+energy_source_controller decision_ctrl (
+    .total_load(total_load),
+    .solar_generation(solar_generation),
+    .battery_status(battery_status),
+    .day_flag(day_flag),
+    .solar_mode(solar_mode),
+    .battery_mode(battery_mode),
+    .grid_mode(grid_mode)
+);
+endmodule
